42eb3cff6ad5877fc72d9c43bd363899ddae8ede
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-07-12 13:04:27 +0100

    domain seession

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
       sameSite:"none",
-      domain:"picturingthis.netlify.app"
+      domain:".netlify.app"
     },
   })
 );
