756bd8b2946851c227f31e370c50ee4d3c134749
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-07-13 14:55:25 +0100

    httponly true

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
       sameSite:"none",
-      domain:".netlify.app"
+      domain:"picturingthis.netlify.app"
     },
   })
 );
