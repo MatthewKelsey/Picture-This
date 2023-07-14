20696d5a0ddd8d9a1aa9c0bb76ccf5a4348018b5
Matthew Kelsey <matthewkelsey1985@gmail.com>
2023-07-14 12:59:10 +0100

    express sessions removed

--- server/router.js
+++ server/router.js
@@ -8,24 +8,24 @@ const albums = require("./Controllers/albums");
 
 //Photo routes
 router.get("/",  images.getPhotos);
-router.post("/upload", authMiddleware, images.uploadPhoto);
-router.delete("/delete", authMiddleware, images.deletePhoto);
-router.put("/like", authMiddleware, images.addLike);
+router.post("/upload",  images.uploadPhoto);
+router.delete("/delete",  images.deletePhoto);
+router.put("/like",  images.addLike);
 
 //login routes
 
 router.post("/register", secure.registerUser);
 router.post("/login", secure.login);
-router.post("/logout", authMiddleware, secure.logout);
-router.get("/users", secure.getUsers);
-router.get("/refresh", authMiddleware, secure.refreshUser);
+router.post("/logout",  secure.logout);
+// router.get("/users", secure.getUsers);
+router.get("/refresh", secure.refreshUser);
 // Album routes
 
-router.post("/newAlbum", authMiddleware, albums.createAlbum);
-router.post("/album", authMiddleware, albums.getAlbum);
-router.post("/share-album", authMiddleware, albums.shareAlbum);
+router.post("/newAlbum",  albums.createAlbum);
+router.post("/album",  albums.getAlbum);
+router.post("/share-album",  albums.shareAlbum);
 router.post("/accept-invite", albums.acceptAlbum);
-router.delete("/album", authMiddleware, albums.deleteAlbum);
-router.put("/album", authMiddleware, albums.removeSharedAlbum);
-router.put("/reject-invite", authMiddleware, albums.rejectAlbum);
+router.delete("/album",  albums.deleteAlbum);
+router.put("/album",  albums.removeSharedAlbum);
+router.put("/reject-invite",  albums.rejectAlbum);
 module.exports = router;
